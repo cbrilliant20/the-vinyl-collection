2a3ae64fc2918fd68bd1c7759875175faa8606ed
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-05-27 15:06:33 -0400

    layout.jsx, footer.jsx, footer.css, index.js (browser router)

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import "./Layout.css"
+import Nav from "../Nav/Nav"
+import Footer from "../Footer/Footer"
+
+const Layout = (props) => {
+  return (
+    <div className="layout">
+      <div className="layout-children">{props.children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
