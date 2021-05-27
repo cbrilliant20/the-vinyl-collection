e895dc3f0e9515ec78f80a8ad9bf0639bd0f5c66
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-05-27 15:51:27 -0400

    productcards comments

---
+++ client/src/components/ProductCards/ProductCards.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import "./ProductCards.css"
+import ProductCard from "../ProductCard/ProductCard"
+import { getProducts } from "../../services/products"
+
+const ProductCards = () => {
+  const [products, setProducts] = useState([])
+
+  // getProducts still needs to be created in services when API is working
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const allProducts = await getProducts()
+      setProducts(allProducts)
+    }
+    fetchProducts()
+  }, [])
+
+  const CARDS = products.map((product, index) => (
+    <ProductCard
+      _id={product._id}
+      name={product.name}
+      imgURL={product.imgURL}
+      key={index}
+    />
+  ))
+
+  return (
+    <div className="product-cards">
+      <div className="cards">{CARDS}</div>
+    </div>
+  )
+}
+
+export default ProductCards
