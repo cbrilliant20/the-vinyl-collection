5828e0376b5da76c16147bf341a5aa0be710d7e4
Nick Kofteris <nkofteris@yahoo.com>
2021-05-28 11:48:21 -0400

    Home Screen and ProductDetail Screen

---
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,54 @@
+import "./ProductDetail.css";
+import { useState, useEffect } from "react";
+import { Layout } from "../../components";
+import { getProduct, deleteProduct } from "../../services/products";
+import { useParams, Link } from "react-router-dom";
+
+const ProductDetail = (props) => {
+  const [product, setProduct] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const product = await getProduct(id);
+      setProduct(product);
+      setLoaded(true);
+    };
+    fetchProduct();
+  }, [id]);
+
+  if (!isLoaded) {
+    return <h1>Loading In Progress...</h1>;
+  }
+
+  return (
+    <Layout user={props.user}>
+      <div className="product-detail">
+        <img
+          className="product-detail-image"
+          src={product.imgURL}
+          alt={product.album}
+        />
+        <div className="detail">
+          <div className="artist">{product.artist}</div>
+          <div className="album">{product.album}</div>
+          <div className="genre">{product.genre}</div>
+          <div className="year">{product.year}</div>
+          <div className="price">{`$${product.price}`}</div>
+          <Link className="edit-button" to={`/products/${product._id}/edit`}>
+            Edit
+          </Link>
+          <button
+            className="delete-button"
+            onClick={() => deleteProduct(product._id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ProductDetail;
