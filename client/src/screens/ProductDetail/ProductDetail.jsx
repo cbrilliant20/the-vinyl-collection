0a1f293e3b3d1509ed5f04022e305e574c662cdb
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-05-28 12:11:15 -0400

    debugged

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -1,25 +1,25 @@
-import "./ProductDetail.css";
-import { useState, useEffect } from "react";
-import { Layout } from "../../components";
-import { getProduct, deleteProduct } from "../../services/products";
-import { useParams, Link } from "react-router-dom";
+import "./ProductDetail.css"
+import { useState, useEffect } from "react"
+import Layout from "../../components/Layout/Layout"
+import { getProduct, deleteProduct } from "../../services/products"
+import { useParams, Link } from "react-router-dom"
 
 const ProductDetail = (props) => {
-  const [product, setProduct] = useState(null);
-  const [isLoaded, setLoaded] = useState(false);
-  const { id } = useParams();
+  const [product, setProduct] = useState(null)
+  const [isLoaded, setLoaded] = useState(false)
+  const { id } = useParams()
 
   useEffect(() => {
     const fetchProduct = async () => {
-      const product = await getProduct(id);
-      setProduct(product);
-      setLoaded(true);
-    };
-    fetchProduct();
-  }, [id]);
+      const product = await getProduct(id)
+      setProduct(product)
+      setLoaded(true)
+    }
+    fetchProduct()
+  }, [id])
 
   if (!isLoaded) {
-    return <h1>Loading In Progress...</h1>;
+    return <h1>Loading In Progress...</h1>
   }
 
   return (
@@ -48,7 +48,7 @@ const ProductDetail = (props) => {
         </div>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default ProductDetail;
+export default ProductDetail
