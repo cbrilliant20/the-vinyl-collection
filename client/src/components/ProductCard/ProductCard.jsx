7efdc89490ed5e955d4d214d16c1e57f32eb82da
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-05-28 11:04:04 -0400

    layout + nav

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom"
 const ProductCard = (props) => {
   return (
     <div className="product-card">
-      <img className="product-card-image" src={props.imgURL} />
+      <Link className="card" to={`/products/${props._id}`}>
+        <img className="product-card-image" src={props.imgURL} />
+      </Link>
       <p className="product-card-artist">{props.artist}</p>
       <div className="product-card-album">{props.album}</div>
       <div className="product-card-genre">{props.genre}</div>
