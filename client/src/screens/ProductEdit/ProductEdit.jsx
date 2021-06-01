4885e148eb1cbae5f1a768dea156993280a38fe1
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-06-01 08:47:12 -0400

    product

--- client/src/screens/ProductEdit/ProductEdit.jsx
+++ client/src/screens/ProductEdit/ProductEdit.jsx
@@ -1,23 +1,23 @@
-import { useState, useEffect } from "react";
-import "./productEdit.css";
-import { useParams, Redirect } from "react-router-dom";
-import Layout from "../../components";
-import { getProduct, updateProduct } from "../../services/products";
+import { useState, useEffect } from "react"
+import "./productEdit.css"
+import { useParams, Redirect } from "react-router-dom"
+import Layout from "../../components"
+import { getProduct, updateProduct } from "../../services/products"
 import deleteProduct from "../../services/products"
 
 const ProductEdit = (props) => {
   const [product, setProduct] = useState({
-    imgURL: '',
-    artist: '',
-    album: '',
-    genre: '',
-    year: '',
-    price: '', 
+    imgURL: "",
+    artist: "",
+    album: "",
+    genre: "",
+    year: "",
+    price: "",
   })
 
   const [isEdited, setEdited] = useState(false)
   let { id } = useParams()
-  
+
   useEffect(() => {
     const grabProduct = async () => {
       const product = await getProduct(id)
@@ -64,61 +64,59 @@ const ProductEdit = (props) => {
           </form>
         </div>
         <form classNAme="product-edit-form" onSubmit={handleSubmit}>
-        <input
-          className="input-artist"
-          placeholder="Artist"
-          value={product.artist}
-          name="artist"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          className="input-album"
-          placeholder="Album Name"
-          value={product.album}
-          name="album"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          className="input-genre"
-          placeholder="Genre"
-          value={product.genre}
-          name="genre"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          className="input-year"
-          placeholder="Year Released"
-          value={product.year}
-          name="year"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          className="input-price"
-          placeholder="Price"
-          value={product.price}
-          name="price"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <button type="submit" className="save-button">
+          <input
+            className="input-artist"
+            placeholder="Artist"
+            value={product.artist}
+            name="artist"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-album"
+            placeholder="Album Name"
+            value={product.album}
+            name="album"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-genre"
+            placeholder="Genre"
+            value={product.genre}
+            name="genre"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-year"
+            placeholder="Year Released"
+            value={product.year}
+            name="year"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-price"
+            placeholder="Price"
+            value={product.price}
+            name="price"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <button type="submit" className="save-button">
             Save Changes
-        </button>
-          <button onClick={(e) => deleteProduct(product._id)}>
-            Delete
           </button>
+          <button onClick={(e) => deleteProduct(product._id)}>Delete</button>
         </form>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default ProductEdit;
+export default ProductEdit
